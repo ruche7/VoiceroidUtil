862d6ed61af63c2f8130e70f62fba62efa7b25f9
ruche7 <pg@ruche-home.net>
2016-02-20 21:44:14 +0900

    XAMLデザイナで表示確認できない問題に対処。

--- VoiceroidUtil/SaveSettingsControl.xaml.cs
+++ VoiceroidUtil/SaveSettingsControl.xaml.cs
@@ -8,11 +8,17 @@ namespace VoiceroidUtil
     /// </summary>
     public partial class SaveSettingsControl : UserControl
     {
+        private bool IsAssemblyLoaded { get; }
+
         /// <summary>
         /// コンストラクタ。
         /// </summary>
         public SaveSettingsControl()
         {
+            this.IsAssemblyLoaded = (
+                typeof(System.Windows.Interactivity.Interaction) != null &&
+                typeof(Microsoft.Expression.Interactivity.VisualStateUtilities) != null);
+
             InitializeComponent();
         }
     }
