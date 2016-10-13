b9672896ade8de8bc99eaec3a782a6d17ec429ec
ruche7 <pg@ruche-home.net>
2016-10-14 07:06:17 +0900

    CanModify と設定ファイル読み書き対応。
    refs #5

--- VoiceroidUtil/ViewModel/MainWindowViewModel.cs
+++ VoiceroidUtil/ViewModel/MainWindowViewModel.cs
@@ -22,17 +22,17 @@ namespace VoiceroidUtil.ViewModel
             this.UIConfig = (new UIConfigViewModel()).AddTo(this.CompositeDisposable);
             this.TalkTextReplaceConfig =
                 (new TalkTextReplaceConfigViewModel()).AddTo(this.CompositeDisposable);
-            this.AppConfig = (new AppConfigViewModel()).AddTo(this.CompositeDisposable);
             this.ExoConfig = (new ExoConfigViewModel()).AddTo(this.CompositeDisposable);
+            this.AppConfig = (new AppConfigViewModel()).AddTo(this.CompositeDisposable);
             this.LastStatus = (new AppStatusViewModel()).AddTo(this.CompositeDisposable);
 
             // Messenger を MainWindowViewModel のもので上書き
             this.Voiceroid.Messenger = this.Messenger;
             this.UIConfig.Messenger = this.Messenger;
             this.TalkTextReplaceConfig.Messenger = this.Messenger;
-            this.AppConfig.Messenger = this.Messenger;
             this.ExoConfig.Messenger = this.Messenger;
             this.ExoConfig.CharaStyle.Messenger = this.Messenger;
+            this.AppConfig.Messenger = this.Messenger;
             this.LastStatus.Messenger = this.Messenger;
 
             // 同期コンテキスト取得
@@ -41,14 +41,14 @@ namespace VoiceroidUtil.ViewModel
             // 同期コンテキスト設定
             this.UIConfig.Value.SynchronizationContext = syncContext;
             this.TalkTextReplaceConfig.Value.SynchronizationContext = syncContext;
-            this.AppConfig.Value.SynchronizationContext = syncContext;
             this.ExoConfig.Value.SynchronizationContext = syncContext;
+            this.AppConfig.Value.SynchronizationContext = syncContext;
 
             // UI設定を UIConfigViewModel のもので上書き
             this.Voiceroid.UIConfig.Value = this.UIConfig.Value;
             this.TalkTextReplaceConfig.UIConfig.Value = this.UIConfig.Value;
-            this.AppConfig.UIConfig.Value = this.UIConfig.Value;
             this.ExoConfig.UIConfig.Value = this.UIConfig.Value;
+            this.AppConfig.UIConfig.Value = this.UIConfig.Value;
             this.UIConfig
                 .ObserveProperty(c => c.Value)
                 .Subscribe(
@@ -60,8 +60,8 @@ namespace VoiceroidUtil.ViewModel
                         // 各 ViewModel に設定
                         this.Voiceroid.UIConfig.Value = c;
                         this.TalkTextReplaceConfig.UIConfig.Value = c;
-                        this.AppConfig.UIConfig.Value = c;
                         this.ExoConfig.UIConfig.Value = c;
+                        this.AppConfig.UIConfig.Value = c;
                     })
                 .AddTo(this.CompositeDisposable);
 
@@ -105,16 +105,16 @@ namespace VoiceroidUtil.ViewModel
                     idle =>
                     {
                         this.TalkTextReplaceConfig.CanModify.Value = idle;
-                        this.AppConfig.CanModify.Value = idle;
                         this.ExoConfig.CanModify.Value = idle;
+                        this.AppConfig.CanModify.Value = idle;
                     })
                 .AddTo(this.CompositeDisposable);
             Observable
                 .Merge(
                     this.Voiceroid.LastStatus,
                     this.TalkTextReplaceConfig.LastStatus,
-                    this.AppConfig.LastStatus,
-                    this.ExoConfig.LastStatus)
+                    this.ExoConfig.LastStatus,
+                    this.AppConfig.LastStatus)
                 .Where(s => s != null)
                 .Subscribe(s => this.LastStatus.Value = s)
                 .AddTo(this.CompositeDisposable);
@@ -153,14 +153,14 @@ namespace VoiceroidUtil.ViewModel
         public TalkTextReplaceConfigViewModel TalkTextReplaceConfig { get; }
 
         /// <summary>
-        /// アプリ設定 ViewModel を取得する。
-        /// </summary>
-        public AppConfigViewModel AppConfig { get; }
-
-        /// <summary>
         /// AviUtl拡張編集ファイル用設定 ViewModel を取得する。
         /// </summary>
         public ExoConfigViewModel ExoConfig { get; }
+
+        /// <summary>
+        /// アプリ設定 ViewModel を取得する。
+        /// </summary>
+        public AppConfigViewModel AppConfig { get; }
 
         /// <summary>
         /// 直近のアプリ状態 ViewModel を取得する。
