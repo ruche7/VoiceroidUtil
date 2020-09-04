54292c45c26619dcb4dcf6b64795f0ca3abf90ea
ruche7 <pg@ruche-home.net>
2020-09-05 04:00:19 +0900

    Windows10 v2004 の新MS-IMEで TextBox の変換時挙動がおかしくなる不具合に対処
    refs #170

---
+++ VoiceroidUtil/View/ImeWatermarkTextBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
+
+namespace VoiceroidUtil.View
+{
+    /// <summary>
+    /// IMEの挙動を考慮した即時反映バインディングを行う、
+    /// <see cref="WatermarkTextBox"/> の派生クラス。
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Windows10 May 2020 Update 以降の新MS-IMEが、 <see cref="Text"/> プロパティに
+    /// UpdateSourceTrigger=PropertyChanged でバインディングされた
+    /// TextBox での変換時におかしな挙動となるため、その対処のために用意したクラス。
+    /// </para>
+    /// <para>
+    /// <see cref="Text"/> プロパティへのバインディング時は
+    /// UpdateSourceTrigger=Explicit とすることを推奨する。
+    /// </para>
+    /// </remarks>
+    public class ImeWatermarkTextBox : WatermarkTextBox
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public ImeWatermarkTextBox()
+        {
+        }
+
+        /// <summary>
+        /// <see cref="Text"/> プロパティの変更時に呼び出される。
+        /// </summary>
+        /// <param name="e">イベント引数。</param>
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            // 自前でバインディングソース更新
+            this.GetBindingExpression(TextProperty).UpdateSource();
+
+            base.OnTextChanged(e);
+        }
+    }
+}
