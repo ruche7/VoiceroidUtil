773dd52b9898c6dc7306b8470e17f28b5a60f74f
ruche7 <pg@ruche-home.net>
2021-03-24 05:55:39 +0900

    アセンブリバージョン更新

--- RucheHome.Voiceroid/Properties/AssemblyInfo.cs
+++ RucheHome.Voiceroid/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Windows.Markup;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.12.0.*")]
+[assembly: AssemblyVersion("1.13.0.*")]
 
 // XML名前空間
 [assembly: XmlnsDefinition(
--- VoiceroidUtil/Properties/AssemblyInfo.cs
+++ VoiceroidUtil/Properties/AssemblyInfo.cs
@@ -49,4 +49,4 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.14.1.*")]
+[assembly: AssemblyVersion("1.15.0.*")]
